5956673860b45db5410f94be6f43af33dd93b9e9
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-05-07 18:09:34 -0300

    implementação do ícone de visualizar senha. > > Co-authored-by: paola-oliveira <annapsjanuario@gmail.com>

--- src/Componentes/CampoTexto/CampoTexto.js
+++ src/Componentes/CampoTexto/CampoTexto.js
@@ -1,15 +1,28 @@
 import './CampoTexto.css'
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const CampoTexto = (props) => {
-    return (
-        <div className='campoTexto'>
-            <label>{props.label}</label> 
-            <div className="inputContainer">
-                {props.icon && <props.icon className="icon" />}
-                <input type={props.type}/>
-            </div>  
-        </div>
-    )
-}
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+  const handleMostrarSenha = () => {
+    setMostrarSenha(!mostrarSenha);
+  };
+
+  return (
+    <div className='campoTexto'>
+      <label>{props.label}</label> 
+      <div className="inputContainer">
+        {props.icon && <props.icon className="icon" />}
+        <input type={mostrarSenha ? 'text' : props.type} />
+        {props.type === 'password' && (
+          <span className="iconeOlho" onClick={handleMostrarSenha}>
+            {mostrarSenha ? < FaEye/> : <FaEyeSlash />}
+          </span>
+        )}
+      </div>  
+    </div>
+  );
+};
 
 export default CampoTexto;
