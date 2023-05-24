25fc35337155cf7d733a04015d491b1711096bb9
paulajanu <annapsjanuario@gmail.com>
2023-05-24 17:27:57 -0300

    filtro dos produtos mais adição de novos produtos. > > Co-authored-by: paola-oliveira <paolanatalia_oliveira@outlook.com>

--- src/Paginas/Pedidos/Pedidos.js
+++ src/Paginas/Pedidos/Pedidos.js
@@ -3,7 +3,6 @@ import queryString from 'query-string';
 import { useLocation } from 'react-router-dom';
 import CardPedido from '../../Componentes/CardPedido/CardPedido.js';
 import CardapioProdutos from '../../Componentes/CardapioProdutos/CardapioProdutos.js';
-import FiltroCardapio from '../../Componentes/FiltroCardapio/FiltroCardapio.js';
 import MesaCliente from '../../Componentes/MesaCliente/MesaCliente.js';
 import Navegador from "../../Componentes/Navegador/Navegador.js";
 import Voltar from '../../Componentes/Voltar/Voltar.js';
@@ -21,7 +20,6 @@ function Pedidos() {
       <div className="pedidos-content">
         <div>
           <Voltar />
-          <FiltroCardapio />
           <CardapioProdutos />
         </div>
         <div>
