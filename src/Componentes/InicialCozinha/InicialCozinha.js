05ca4b17e426d64742992b5b99bc96ae56505060
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-06-07 17:47:08 -0300

    inicio da customizacao dos cards de pedidos. > > Co-authored-by: paulajanu <annapsjanuario@gmail.com>

--- src/Componentes/InicialCozinha/InicialCozinha.js
+++ src/Componentes/InicialCozinha/InicialCozinha.js
@@ -2,67 +2,71 @@ import './InicialCozinha.css';
 import Menu from '../Menu/Menu';
 import { obterPedidos } from '../../API/api';
 import { useEffect, useState } from 'react';
+import Botao from '../Botao/Botao';
 
 const InicialCozinha = () => {
+  const [pedidos, setPedidos] = useState([]);
 
-    const [pedidos, setPedidos] = useState([]);
+  useEffect(() => {
+    const fetchPedidos = async () => {
+      try {
+        const response = await obterPedidos();
 
-    useEffect(() => {
-        const fetchPedidos = async () => {
-          try {
-            const response = await obterPedidos();
-      
-            if (response.ok) {
-              const data = await response.json();
-              setPedidos(data);
-            } else {
-              console.error('Erro ao obter os pedidos da API');
-            }
-          } catch (error) {
-            console.error('Erro ao obter os pedidos da API', error);
-          };
-        };
-      
-        fetchPedidos();
-    }, []);
+        if (response.ok) {
+          const data = await response.json();
+          setPedidos(data);
+        } else {
+          console.error('Erro ao obter os pedidos da API');
+        }
+      } catch (error) {
+        console.error('Erro ao obter os pedidos da API', error);
+      }
+    };
 
-    return (
-        <main className='menu-cozinha'>
-        <div className="menu-cozinha-coluna">
-            <Menu imagem="/imagens/em-preparo.png" texto="EM PRODUÇÃO"/>
-            <Menu imagem="/imagens/em-preparo2.png" texto="EM PREPARO"/>
-            <Menu imagem="/imagens/prontos.png" texto="PRONTOS"/>
-        </div>
+    fetchPedidos();
+  }, []);
 
-        <div className='pedidos'>
-      {pedidos.length === 0 ? (
-        <div>
-          <p className="pedidos-instrucao">Sem pedidos no momento!</p>
-          <div className='div-img-sem-pedidos'>
-            <img className='img-sem-pedidos' src="/imagens/sem-pedidos.png" alt="Imagem de sem pedidos" />
+  return (
+    <main className='menu-cozinha'>
+      <div className="menu-cozinha-coluna">
+        <Menu imagem="/imagens/em-preparo.png" texto="EM PRODUÇÃO" />
+        <Menu imagem="/imagens/em-preparo2.png" texto="EM PREPARO" />
+        <Menu imagem="/imagens/prontos.png" texto="PRONTOS" />
+      </div>
+
+      <div className='pedidos'>
+        {pedidos.length === 0 ? (
+          <div>
+            <p className="pedidos-instrucao">Sem pedidos no momento!</p>
+            <div className='div-img-sem-pedidos'>
+              <img className='img-sem-pedidos' src="/imagens/sem-pedidos.png" alt="Imagem de sem pedidos" />
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="cards-container">
-      {pedidos.map((pedido) => (
-        <div className="pedido-card" key={pedido.id}>
-          <p>Cliente: {pedido.client}</p>
-          <p>Data de entrada: {pedido.dateEntry}</p>
-          <div className="produtos-container">
-            <p>Resumo do Pedido</p>
-            {pedido.products.map((produto) => (
-              <div className="produto-card" key={produto.id}>
-                <p>{produto.quantity} - {produto.name}</p>
+        ) : (
+          pedidos.map((pedido) => (
+            <div className="cards-pedidos" key={pedido.id}>
+                <div className="pedido-card">
+                <p className='pedido'>Cliente: {pedido.client}</p>
+                <p className='pedido'>Data de entrada: {pedido.dateEntry}</p>
+                <div className="produtos-container">
+                  <p className='titulo-pedido'>Resumo do Pedido</p>
+                  {pedido.products.map((produto) => (
+                    <div className="produto-card" key={produto.id}>
+                      <p className='pedido'>{produto.quantity} - {produto.name}</p>
+                    </div>
+                  ))}
+                </div>
+              <div className='botoes-pedidos'>
+                <Botao className="confirmar-cancelar verde">PREPARAR</Botao>
+                <Botao className="confirmar-cancelar vermelho">CANCELAR</Botao>
+                </div>
               </div>
-            ))}
-          </div>
-        </div>
-      ))}
-    </div>
-  )}
-    </div>
-         </main>
-)
-}
+            </div>
+          ))
+        )}
+      </div>
+    </main>
+  );
+};
 
 export default InicialCozinha;
