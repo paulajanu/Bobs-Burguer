b28e2be60aa0b1a7c26b5c6429513553d599d649
paulajanu <annapsjanuario@gmail.com>
2023-06-07 16:05:27 -0300

    Inicialização da terceira história de usuário. > > Co-authored-by: paola-oliveira <paolanatalia_oliveira@outlook.com>

--- src/API/api.js
+++ src/API/api.js
@@ -4,20 +4,20 @@ const APIURL = 'https://burger-queen-api-mock-alpha.vercel.app';
 
 export default function login(email, senha) {
     // console.log('Chamando a função login com email:', email, 'e senha:', senha);
-    return fetch(`${APIURL}/login`, {
-        // Tipo da requisicao
-        method: "POST",
-        //o headers indicar que estamos enviando dados no formato JSON
-        //o content type serve para indicar o tipo de dado que estamos enviando.
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        // Dados a serem enviados na requisicao
-        body: JSON.stringify({
-            email: email,
-            password: senha
-        })
-    });
+  return fetch(`${APIURL}/login`, {
+    // Tipo da requisicao
+    method: "POST",
+    //o headers indicar que estamos enviando dados no formato JSON
+    //o content type serve para indicar o tipo de dado que estamos enviando.
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // Dados a serem enviados na requisicao
+    body: JSON.stringify({
+      email: email,
+      password: senha
+    })
+  });
 };
 
 export const obterProdutos = () => {
@@ -32,26 +32,36 @@ export const obterProdutos = () => {
 
 export const enviarPedido = (idUsuario, cliente, arrayProdutos, dataEntrada) => {
 
-    return fetch(`${APIURL}/orders`, {
-      method: "POST",
+  return fetch(`${APIURL}/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${getItens()}`,
+    },
+    body: JSON.stringify({
+      userId: idUsuario,
+      client: cliente,
+      products: arrayProdutos,
+      status: "pending",
+      dateEntry: dataEntrada,
+    }),
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => {
+      console.log(error)
+      throw error
+    });
+};
+
+export const obterPedidos = () => {
+  return fetch(`${APIURL}/orders`, {
+      method: "GET",
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${getItens()}`,
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${getItens()}`,
       },
-      body: JSON.stringify({
-        userId: idUsuario,
-        client: cliente,
-        products: arrayProdutos,
-        status: "pending",
-        dateEntry: dataEntrada,
-      }),
-    })
-      .then(response => response.json()) // Converter a resposta em json
-      .then(json => console.log(json))
-      .catch(error => {
-        console.log(error)
-        throw error
-      });
-  };
+  });
+};
 
   
